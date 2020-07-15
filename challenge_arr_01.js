804bf8d6c82458830b7a7a056169e8d8d4602bce
Peter Schmidt <schmidt.multimedia@gmail.com>
2020-07-15 10:02:53 +0200

    Add files via upload

---
+++ challenge_arr_01.js
@@ -0,0 +1,12 @@
+/*********   CODE CHALLENGE Arrays **********/
+
+/*
+Aufgabe:
+Erstellen Sie eine JS-Struktur, die Ihnen den folgenden String einer HTML-Seite ausgibt:
+<html><head></head><body><p></p></body></html>
+Verwenden Sie dafür die untenstehenden Arrays
+*/
+
+var controls = ['<','>','/'];
+var elements = ['html','head','head','body','p','p','body','html'];
+
